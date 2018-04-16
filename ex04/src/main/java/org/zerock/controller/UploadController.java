a07555e43070e34d0c0181b4022daa2829c5e97c
Kim Ah Hyoung <llmooon@naver.com>
2018-04-17 01:37:20 +0900

    Add files via upload

---
+++ ex04/src/main/java/org/zerock/controller/UploadController.java
@@ -0,0 +1,69 @@
+package org.zerock.controller;
+
+import java.io.File;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class UploadController {
+   private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+   
+   @Resource(name = "uploadPath")
+   private String uploadPath;
+   
+   @RequestMapping(value = "/uploadForm", method = RequestMethod.GET)
+   public void uploadForm() {
+      
+   }
+   
+   @RequestMapping(value = "/uploadForm", method = RequestMethod.POST)
+   public String uploadForm(MultipartFile file, Model model) throws Exception {
+      logger.info("originalName : " + file.getOriginalFilename());
+      logger.info("size : " + file.getSize());
+      logger.info("contentType : " + file.getContentType());
+      
+      String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
+      
+      model.addAttribute("savedName", savedName);
+      
+      return "uploadResult";
+   }
+   
+   @RequestMapping(value = "/uploadAjax", method = RequestMethod.GET)
+   public void uploadAjax() {
+      
+   }
+
+   @ResponseBody
+   @RequestMapping(value="/uploadAjax", method=RequestMethod.POST, produces="text/plain;charset=UTF-8")
+   public ResponseEntity<String> uploadAjax(MultipartFile file) throws Exception{
+	   logger.info("originalName : " + file.getOriginalFilename());
+	   logger.info("size : " + file.getSize());
+	   logger.info("contentType : " + file.getContentType()); 
+	   String savedName = uploadFile(file.getOriginalFilename(), file.getBytes());
+	   return new ResponseEntity<String>(file.getOriginalFilename(),HttpStatus.CREATED);
+   }
+   
+   private String uploadFile(String originalName, byte[] fileData) throws Exception {
+      // TODO Auto-generated method stub
+      UUID uid = UUID.randomUUID();
+      String savedName = uid.toString() + "_" + originalName;
+      File target = new File(uploadPath, savedName);
+      FileCopyUtils.copy(fileData, target);
+      
+      return savedName;
+   }
+}
